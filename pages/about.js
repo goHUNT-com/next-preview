e6baacafa8159e4c6eef5a38bdc33af93c483a80
Justin Mitchell <justin@gohunt.com>
2023-03-28 14:14:41 -0700

    initial

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+const About = () => {
+    return (
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="keywords" content="ninjas" />
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Bacon ipsum dolor amet pancetta meatloaf swine picanha hamburger, ground round tri-tip capicola. Turducken kevin sausage, bresaola andouille shoulder shankle venison tri-tip pork loin ham tenderloin filet mignon porchetta drumstick</p>
+                <p>Bacon ipsum dolor amet pancetta meatloaf swine picanha hamburger, ground round tri-tip capicola. Turducken kevin sausage, bresaola andouille shoulder shankle venison tri-tip pork loin ham tenderloin filet mignon porchetta drumstick</p>
+            </div>
+        </>
+      );
+}
+ 
+export default About;
